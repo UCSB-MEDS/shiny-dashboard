8cebfdf885607950e20be39aa2d88615895f20b6
hdolinh <halina@bren.ucsb.edu>
2022-07-12 14:20:59 -0700

    added menu items to sidebar

--- ui.R
+++ ui.R
@@ -5,10 +5,26 @@ dashboardPage(
     title = "Bren Dashboard"
   ), # EO dashboardHeader
   dashboardSidebar(
-    title = "Menu"
+    id = "tabs", # Note(HD): not sure why tabs is showing up?
+    title = "Menu",
+    menuItem(id = "current_yr",
+             text = "2021 Dashboard", # Note(HD): difference btwn text and tabName arg?
+             icon = icon("dashboard")
+             ), # EO current_yr menuItem
+    menuItem(id = "career_db",
+             text = "Career Outcomes",
+             icon = icon("th")
+             ), # EO career_db menuItem
+    menuItem(id = "student_demo",
+             text = "Student Demographics",
+             icon = icon("circle"))
+    
+    
   ), # EO dashboardSideBar
   
   dashboardBody(
+    
+    
     
   ) # EO dashboardBody
 ) # EO dashboardPage
