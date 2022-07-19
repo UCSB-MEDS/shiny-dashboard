12b5bc229ce49c1fb932a20baa8745da74229e2b
hdolinh <halina@bren.ucsb.edu>
2022-07-19 09:18:20 -0700

    redo ui to figure out why content wasn't showing up for menuItem

--- server.R
+++ server.R
@@ -1,11 +1,11 @@
 # server instructions
 server <- function(input, output, session){
   
-  observeEvent(input$sidebarItemExpanded, {
-    if(input$sidebarItemExpanded == "CAREER"){
-      updateTabItems(session, "tabs", selected = "hidden_career")
-    }
-    
-  }) # EO OE
+  # observeEvent(input$career_db, {
+  #   if(input$career_db == "CAREER"){
+  #     updateTabItems(session, "tabs", selected = "hidden_career")
+  #   }
+  # 
+  # }) # EO OE
   
 } # EO server
--- ui.R
+++ ui.R
@@ -8,76 +8,62 @@ dashboardPage(
   
   # SIDEBAR ----
   dashboardSidebar(
+    id = "tabs",
     title = "Menu",
     sidebarMenu(
-      # Setting id makes input$tabs give the tabName of currently-selected tab
-      id = "tabs",
+      # curr yr ----
+      menuItem(
+        tabName = "curr_yr",
+        text = "2021 Data",
+        icon = icon("dashboard")
+      ), # EO curr yr menuItem
       
-      ## Current yr ----
-      menuItem(id = "current_yr",
-               tabName = "current_yr",
-               text = "2021 Dashboard",
-               icon = icon("dashboard")
-      ), # EO current_yr menuItem
+      # career ----
+      menuItem(
+        tabName = "career_home",
+        text = "Career Outcomes 101",
+        icon = icon("th"),
+        
+        # curr career ----
+        menuSubItem(
+          tabName = "curr_career",
+          text = "Current Career Data"
+        ), # EO curr_career menuSubItem
+        
+        # 5 yr career ----
+        menuSubItem(
+          tabName = "career_5yr",
+          text = "5 Year Career Data"
+        ) # EO 5 yr career
+      ) # EO career menuItem
       
-      ## Career db ----
-      menuItem(id = "career_db",
-               tabName = "career_db",
-               text = "Career Outcomes",
-               icon = icon("th"),
-               expandedName = "CAREER",
-               ### sub current yr ----
-               menuSubItem(text = "Currrent Year",
-                           tabName = "career_curryr"),
-               
-               ### sub 5 yr ----
-               menuSubItem(text = "5 Year Trends",
-                           tabName = "career_5yr")
-      ), # EO career_db menuItem
-      
-      ## Student demo ----
-      menuItem(id = "student_demo",
-               tabName = "student_demo",
-               text = "Student Demographics",
-               icon = icon("circle"),
-               ### sub current yr ----
-               menuSubItem(text = "Currrent Year",
-                           tabName = "demo_curryr"),
-               
-               ### sub 5 yr ----
-               menuSubItem(text = "5 Year Trends",
-                           tabName = "demo_5yr")
-      ), # EO student_demo menuItem
-      
-      shinyjs::hidden(menuItem(id = "hidden_career", 
-                               tabName = "hidden_career",
-                               text = "test"))
-    ) # EO tabs sidebarMenu
-  ), # EO dashboardSideBar
+    ) # EO sidebarMenu
+  ), # EO dashboardSidebar
   
   # BODY ----
   dashboardBody(
-    useShinyjs(),
     tabItems(
       # Note(HD): tabName must match tabName in menuItem
-      tabItem(tabName = "current_yr",
-              h2("Current year dashboard content")
-      ), # EO current_yr tabItem
-      tabItem(tabName = "hidden_career",
-              h2("Career dashboard content")
-      ), # EO career_db tabItem
-      tabItem(tabName = "career_curryr",
-              h2("Career current year dashboard content")
-      ), # EO career_curryr tabItem
-      tabItem(tabName = "career_5yr",
-              h2("Career 5 year dashboard content")
-      ), # EO career_5yr tabItem
-      tabItem(tabName = "demo_curryr",
-              h2("Student Demographics current year dashboard content")
-      ), # EO demo_curryr tabItem
-      tabItem(tabName = "demo_5yr",
-              h2("Student Demographics 5 year dashboard content")
-      ) # EO demo_5yr tabItem
+      tabItem(
+        tabName = "curr_yr",
+        h2("2021 Dashboard Content")
+      ), # EO curr yr tabItem
+      
+      tabItem(
+        tabName = "career_home",
+        h2("Career Outcomes Home Content")
+      ), # EO career home tabItem
+      
+      tabItem(
+        tabName = "curr_career",
+        "Current Career Content"
+      ), # EO curr career tabItem
+      
+      tabItem(
+        tabName = "career_5yr",
+        "5 yr Career Content"
+      ) # EO 5 yr career tabItem
     ) # EO tabItems
+    
   ) # EO dashboardBody
 ) # EO dashboardPage
