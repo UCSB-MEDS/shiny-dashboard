a184ae52cec3c4acd93023a53efea4640d6dc83f
hdolinh <halina@bren.ucsb.edu>
2022-07-29 14:09:39 -0700

    removed larger datasets; added smaller ones

--- global.R
+++ global.R
@@ -15,11 +15,13 @@ library(d3treeR) # interactive tree map
 library(shinycssloaders) # loading icon
 
 # IMPORT DATA ----
-apps_clean <- readRDS("data/apps_all_clean.rds")
+admissions <- readRDS("data/admissions.rds")
 bren_apps <- readRDS("data/bren_apps.rds")
 mesmP <- readRDS("data/mesmP_all_clean.rds")
 mesmS <- readRDS("data/mesmS_all_clean.rds")
 diversity <- readRDS("data/diversity_all.rds")
+origins_df <- readRDS("data/origins_df.rds")
+df_state_geometries_us <- readRDS("data/df_state_geometries_us.rds")
 
 # SOURCE FUNCTIONS ----
 
@@ -32,13 +34,3 @@ prog_cohort_tot_all <- bren_apps %>%
   group_by(objective1, ay_year) %>%
   summarize(cohort_tot = n())
 
-# get state geometries using tigris
-df_state_geometries_us <- tigris::states(year = 2018) %>%
-  select(GEOID, STUSPS, NAME, geometry) %>% 
-  rename(fips = GEOID,
-         state_abbrev = STUSPS,
-         state = NAME) %>% 
-  rmapshaper::ms_simplify(keep = 0.005, keep_shapes = TRUE)
-
-# get international country geometries
-world <- st_read(system.file("shapes/world.gpkg", package="spData"))
