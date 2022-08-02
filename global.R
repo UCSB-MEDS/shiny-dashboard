10541879f6b97245976235ce3e77406ee3574829
hdolinh <halina@bren.ucsb.edu>
2022-08-01 17:01:24 -0700

    added waffle package to create waffle plot

--- global.R
+++ global.R
@@ -13,6 +13,7 @@ library(plotly)
 library(treemap)
 library(d3treeR) # interactive tree map
 library(shinycssloaders) # loading icon
+library(waffle)
 
 # IMPORT DATA ----
 admissions <- readRDS("data/admissions.rds")
